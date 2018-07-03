d25ff8d66772ae8453b8465e37ea784d5bc4afbc
yutopp <yutopp@gmail.com>
2018-07-03 19:03:42 +0900

    server_demo: divide files. add flv recording features

---
+++ example/server_demo/handler.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/yutopp/go-flv"
+	flvtag "github.com/yutopp/go-flv/tag"
+	rtmpmsg "github.com/yutopp/go-rtmp/message"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Handler struct {
+	flvFile *os.File
+	flvEnc  *flv.Encoder
+}
+
+func (h *Handler) OnConnect(timestamp uint32, cmd *rtmpmsg.NetConnectionConnect) error {
+	log.Printf("OnConnect: %+v", cmd)
+	return nil
+}
+
+func (h *Handler) OnPublish(timestamp uint32, cmd *rtmpmsg.NetStreamPublish) error {
+	log.Printf("OnPublish: %+v", cmd)
+
+	// record streams as FLV!
+	p := filepath.Join(
+		os.TempDir(),
+		filepath.Clean(filepath.Join("/", fmt.Sprintf("%s.flv", cmd.PublishingName))),
+	)
+	f, err := os.OpenFile(p, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return errors.Wrap(err, "Failed to create flv file")
+	}
+	h.flvFile = f
+
+	enc, err := flv.NewEncoder(f, flv.FlagsAudio|flv.FlagsVideo)
+	if err != nil {
+		_ = f.Close()
+		return errors.Wrap(err, "Failed to create flv encoder")
+	}
+	h.flvEnc = enc
+
+	return nil
+}
+
+func (h *Handler) OnPlay(timestamp uint32, args []interface{}) error {
+	return errors.New("Not supported")
+}
+
+func (h *Handler) OnSetDataFrame(timestamp uint32, payload []byte) error {
+	r := bytes.NewReader(payload)
+
+	var script flvtag.ScriptData
+	if err := flvtag.DecodeScriptData(r, &script); err != nil {
+		log.Printf("Failed to decode script data: Err = %+v", err)
+		return nil // ignore
+	}
+
+	log.Printf("SetDataFrame: Script = %+v", script)
+
+	if err := h.flvEnc.Encode(&flvtag.FlvTag{
+		TagType:   flvtag.TagTypeScriptData,
+		Timestamp: timestamp,
+		Data:      &script,
+	}); err != nil {
+		log.Printf("Failed to write script data: Err = %+v", err)
+	}
+
+	return nil
+}
+
+func (h *Handler) OnAudio(timestamp uint32, payload []byte) error {
+	r := bytes.NewReader(payload)
+
+	var audio flvtag.AudioData
+	if err := flvtag.DecodeAudioData(r, &audio); err != nil {
+		return err
+	}
+
+	log.Printf("FLV Audio Data: Timestamp = %d, SoundFormat = %+v, SoundRate = %+v, SoundSize = %+v, SoundType = %+v, AACPacketType = %+v, Data length = %+v",
+		timestamp,
+		audio.SoundFormat,
+		audio.SoundRate,
+		audio.SoundSize,
+		audio.SoundType,
+		audio.AACPacketType,
+		len(audio.Data),
+	)
+
+	if err := h.flvEnc.Encode(&flvtag.FlvTag{
+		TagType:   flvtag.TagTypeAudio,
+		Timestamp: timestamp,
+		Data:      &audio,
+	}); err != nil {
+		log.Printf("Failed to write audio: Err = %+v", err)
+	}
+
+	return nil
+}
+
+func (h *Handler) OnVideo(timestamp uint32, payload []byte) error {
+	r := bytes.NewReader(payload)
+
+	var video flvtag.VideoData
+	if err := flvtag.DecodeVideoData(r, &video); err != nil {
+		return err
+	}
+
+	log.Printf("FLV Video Data: Timestamp = %d, FrameType = %+v, CodecID = %+v, AVCPacketType = %+v, CT = %+v, Data length = %+v",
+		timestamp,
+		video.FrameType,
+		video.CodecID,
+		video.AVCPacketType,
+		video.CompositionTime,
+		len(video.Data),
+	)
+
+	if err := h.flvEnc.Encode(&flvtag.FlvTag{
+		TagType:   flvtag.TagTypeVideo,
+		Timestamp: timestamp,
+		Data:      &video,
+	}); err != nil {
+		log.Printf("Failed to write video: Err = %+v", err)
+	}
+
+	return nil
+}
+
+func (h *Handler) OnClose() {
+	log.Printf("OnClose")
+
+	if h.flvFile != nil {
+		_ = h.flvFile.Close()
+	}
+}
--- example/server_demo/main.go
+++ example/server_demo/main.go
@@ -1,89 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"github.com/pkg/errors"
-	flvtag "github.com/yutopp/go-flv/tag"
 	"github.com/yutopp/go-rtmp"
-	rtmpmsg "github.com/yutopp/go-rtmp/message"
 	"log"
 	"net"
 )
-
-type Handler struct {
-}
-
-func (h *Handler) OnConnect(timestamp uint32, cmd *rtmpmsg.NetConnectionConnect) error {
-	log.Printf("OnConnect: %+v", cmd)
-	return nil
-}
-
-func (h *Handler) OnPublish(timestamp uint32, cmd *rtmpmsg.NetStreamPublish) error {
-	log.Printf("OnPublish: %+v", cmd)
-
-	return nil
-}
-
-func (h *Handler) OnPlay(timestamp uint32, args []interface{}) error {
-	return errors.New("Not supported")
-}
-
-func (h *Handler) OnSetDataFrame(timestamp uint32, payload []byte) error {
-	buf := bytes.NewReader(payload)
-	var script flvtag.ScriptData
-	if err := flvtag.DecodeScriptData(buf, &script); err != nil {
-		return errors.Wrap(err, "Failed to decode script data")
-	}
-
-	log.Printf("SetDataFrame: Script = %+v", script)
-
-	return nil
-}
-
-func (h *Handler) OnAudio(timestamp uint32, payload []byte) error {
-	buf := bytes.NewBuffer(payload)
-	audio := &flvtag.AudioData{}
-	err := flvtag.DecodeAudioData(buf, audio)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("FLV Audio Data: Timestamp = %d, SoundFormat = %+v, SoundRate = %+v, SoundSize = %+v, SoundType = %+v, AACPacketType = %+v, Data length = %+v",
-		timestamp,
-		audio.SoundFormat,
-		audio.SoundRate,
-		audio.SoundSize,
-		audio.SoundType,
-		audio.AACPacketType,
-		len(audio.Data),
-	)
-
-	return nil
-}
-
-func (h *Handler) OnVideo(timestamp uint32, payload []byte) error {
-	buf := bytes.NewBuffer(payload)
-	video := &flvtag.VideoData{}
-	err := flvtag.DecodeVideoData(buf, video)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("FLV Video Data: Timestamp = %d, FrameType = %+v, CodecID = %+v, AVCPacketType = %+v, CT = %+v, Data length = %+v",
-		timestamp,
-		video.FrameType,
-		video.CodecID,
-		video.AVCPacketType,
-		video.CompositionTime,
-		len(video.Data),
-	)
-
-	return nil
-}
-
-func (h *Handler) OnClose() {
-	log.Printf("OnClose")
-}
 
 func main() {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", ":1935")
