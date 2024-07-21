95f4555a53b11d4d4cfe17708aa6425ffae2cee0
Dmitrii Okunev <xaionaro@dx.center>
2024-07-21 20:24:58 +0100

    Add missing "omitempty"

--- message/net_connection.go
+++ message/net_connection.go
@@ -26,16 +26,16 @@ type NetConnectionConnect struct {
 }
 
 type NetConnectionConnectCommand struct {
-	App            string       `mapstructure:"app" amf0:"app"`
-	Type           string       `mapstructure:"type" amf0:"type"`
-	FlashVer       string       `mapstructure:"flashVer" amf0:"flashVer"`
-	TCURL          string       `mapstructure:"tcUrl" amf0:"tcUrl"`
-	Fpad           bool         `mapstructure:"fpad" amf0:"fpad"`
-	Capabilities   int          `mapstructure:"capabilities" amf0:"capabilities"`
-	AudioCodecs    int          `mapstructure:"audioCodecs" amf0:"audioCodecs"`
-	VideoCodecs    int          `mapstructure:"videoCodecs" amf0:"videoCodecs"`
-	VideoFunction  int          `mapstructure:"videoFunction" amf0:"videoFunction"`
-	ObjectEncoding EncodingType `mapstructure:"objectEncoding" amf0:"objectEncoding"`
+	App            string       `mapstructure:"app,omitempty" amf0:"app"`
+	Type           string       `mapstructure:"type,omitempty" amf0:"type"`
+	FlashVer       string       `mapstructure:"flashVer,omitempty" amf0:"flashVer"`
+	TCURL          string       `mapstructure:"tcUrl,omitempty" amf0:"tcUrl"`
+	Fpad           bool         `mapstructure:"fpad,omitempty" amf0:"fpad"`
+	Capabilities   int          `mapstructure:"capabilities,omitempty" amf0:"capabilities"`
+	AudioCodecs    int          `mapstructure:"audioCodecs,omitempty" amf0:"audioCodecs"`
+	VideoCodecs    int          `mapstructure:"videoCodecs,omitempty" amf0:"videoCodecs"`
+	VideoFunction  int          `mapstructure:"videoFunction,omitempty" amf0:"videoFunction"`
+	ObjectEncoding EncodingType `mapstructure:"objectEncoding,omitempty" amf0:"objectEncoding"`
 }
 
 func (t *NetConnectionConnect) FromArgs(args ...interface{}) error {
